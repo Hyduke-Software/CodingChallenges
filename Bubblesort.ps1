b45c63a7402ba78432f0e2b3f0118a0eae549fe1
Hyduke <20604455+Hyduke-Software@users.noreply.github.com>
2021-02-01 15:37:34 +0000

    Update Bubblesort.ps1
    Formatting has been updated

--- Bubblesort.ps1
+++ Bubblesort.ps1
@@ -1,23 +1,20 @@
-﻿#to order smallest to largest
-[array]$numbers = 155,88,69,102,5,8,0, 2
-$changecount=0
-write-host $numbers -foregroundcolor blue
-    for ($i=0; $i -lt $numbers.length; $i++){
-   
-        if ($numbers[$i] -gt $numbers[$i+1]){                
-                $temp = $numbers[$i+1]
-                $numbers[$i+1] = $numbers[$i]
-                $numbers[$i] = $temp
-                $changecount++
-                $i = $i-3
-                        if($changecount -gt $numbers.length*$numbers.length +1){
-
-                            write-host $numbers -foregroundcolor green
-                            break
-                                }
-                }
-
+#to order smallest to largest
+[array] $numbers = 155, 88, 69, 102, 5, 8, 0, 2
+$changecount = 0
+write - host $numbers - foregroundcolor blue
+for ($i = 0; $i - lt $numbers.length; $i++) {
+    if ($numbers[$i] - gt $numbers[$i + 1]) {
+        $temp = $numbers[$i + 1]
+        $numbers[$i + 1] = $numbers[$i]
+        $numbers[$i] = $temp
+        $changecount++
+        $i = $i - 3
+        if ($changecount - gt $numbers.length * $numbers.length + 1) {
+            write - host $numbers - foregroundcolor green
+            break
+        }
+    }
 }
 #output:
 #155 88 69 102 5 8 0 2
-#0 2 5 8 69 88 102 155
+# 0 2 5 8 69 88 102 155
