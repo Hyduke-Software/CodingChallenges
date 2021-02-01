19f2835d925f7e5ace3d3aa65409694fb98e4b3b
Hyduke <20604455+Hyduke-Software@users.noreply.github.com>
2021-02-01 15:39:46 +0000

    Update Bubblesort.ps1
    Format update

--- Bubblesort.ps1
+++ Bubblesort.ps1
@@ -1,16 +1,16 @@
 #to order smallest to largest
-[array] $numbers = 155, 88, 69, 102, 5, 8, 0, 2
+[array]$numbers = 155, 88, 69, 102, 5, 8, 0, 2
 $changecount = 0
-write - host $numbers - foregroundcolor blue
-for ($i = 0; $i - lt $numbers.length; $i++) {
-    if ($numbers[$i] - gt $numbers[$i + 1]) {
+write-host $numbers -foregroundcolor blue
+for ($i = 0; $i -lt $numbers.length; $i++) {
+    if ($numbers[$i] -gt $numbers[$i + 1]) {
         $temp = $numbers[$i + 1]
         $numbers[$i + 1] = $numbers[$i]
         $numbers[$i] = $temp
         $changecount++
         $i = $i - 3
-        if ($changecount - gt $numbers.length * $numbers.length + 1) {
-            write - host $numbers - foregroundcolor green
+        if ($changecount -gt $numbers.length * $numbers.length + 1) {
+            write-host $numbers -foregroundcolor green
             break
         }
     }
